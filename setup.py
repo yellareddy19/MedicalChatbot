fb6fea9736a021570bda986b5031984313c4c00a
Yella Reddy <yellareddy1911@gmail.com>
2025-12-17 18:04:29 -0600

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Boktiar Ahmed Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
